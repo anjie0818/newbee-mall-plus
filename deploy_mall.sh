a4e523545aeb3fb2be81dd110ae0eff35f39e839
anjie0818 <anjiework@gmail.com>
2023-11-01 10:31:08 +0800

    test
    Signed-off-by: An.Jie <anjiework@gmail.com>

--- deploy_mall.sh
+++ deploy_mall.sh
@@ -60,7 +60,6 @@ nohup java -Xms512m -Xmx512m -Duser.timezone=GMT+8 -Dfile.encoding=utf-8 \
   -Dskywalking.agent.service_name=$app_name \
   -Dskywalking.agent.instance_name=$app_name-$export_port \
   -Dskywalking.collector.backend_service=$DB_IP:11800 \
-  -agentlib:jdwp=transport=dt_socket,server=y,suspend=n,address=*:1000 \
   -jar ./target/newbee-mall-plus.jar --server.port=$export_port \
   >>/dev/null 2>&1 &
 #java -Xms512m -Xmx512m -Duser.timezone=GMT+8 -Dfile.encoding=utf-8 \
